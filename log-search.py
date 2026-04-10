567244c77a1fce9e1905c206bccace5349e3e1c8
Arthurmystic <188759832+Arthurmystic@users.noreply.github.com>
2026-04-10 21:46:47 +0930

    Add joint search log

--- log-search.py
+++ log-search.py
@@ -12,4 +12,19 @@ def search(filename, text):
         #         result.append(line)
         # print(result)
 
-search('secure.log', 'nobody')
+result2 = []
+
+def joint_search(filename, text, text2):
+    with open(filename) as f:
+      searched_text = [line for line in f if text in line and text2 in line]
+      result2.append(searched_text)
+      print (result2)
+        # for line in f:
+        #     if text in line and text2 in line:
+        #         print(line)
+        #         result2.append(line)
+
+joint_search('Secure.log', 'Failed', '3187')
+
+
+# search('secure.log', 'nobody')
